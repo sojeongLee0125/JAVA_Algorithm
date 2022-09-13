fc78ec65cc231d00beeb510b1b45def97f5b5153
sojeonglee <sojeonglee0125@gmail.com>
2022-09-14 08:18:28 +0900

    [solved] 인프런 자바 알고리즘 문제풀이 : String-6

---
+++ src/String/String_6.java
@@ -0,0 +1,21 @@
+package String;
+
+import java.util.Scanner;
+
+public class String_6 {
+    static String solution(String str) {
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+    }
+}
