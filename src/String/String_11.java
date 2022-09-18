b572e0570f304af0b83a3080e0491d671a905f73
sojeonglee <sojeonglee0125@gmail.com>
2022-09-19 07:09:51 +0900

    [solved] 인프런 자바 알고리즘 문제풀이 : String-11

---
+++ src/String/String_11.java
@@ -0,0 +1,32 @@
+package String;
+
+import java.util.Scanner;
+
+public class String_11 {
+    static String solution(String str) {
+        String answer = "";
+        // 마지막 빈 문자열 추가
+        str += " ";
+        int cnt = 1;
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.charAt(i) == str.charAt(i + 1)) {
+                cnt++;
+            } else {
+                answer += str.charAt(i);
+                if (cnt > 1) {
+                    answer += String.valueOf(cnt);
+                    cnt = 1;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+    }
+}
