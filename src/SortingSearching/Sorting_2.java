83e794dcb771c8108fcf06c38819dc3a61a3479a
sojeonglee <sojeonglee0125@gmail.com>
2022-10-03 11:45:47 +0900

    [solved] 인프런 자바 알고리즘 문제풀이 : Sorting-2(버블정렬)

---
+++ src/SortingSearching/Sorting_2.java
@@ -0,0 +1,32 @@
+package SortingSearching;
+
+import java.util.Scanner;
+
+public class Sorting_2 {
+    public static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int i : solution(n, arr)) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
